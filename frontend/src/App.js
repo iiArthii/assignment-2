752ca7dfd5459fd64d9cf2ad9a52e23272cce956
Arth <88418347+iiArthii@users.noreply.github.com>
2023-10-18 00:01:14 +1100

    app changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,65 @@
-import { useState, useEffect } from 'react';  // import useEffect
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+import CreateContact from './CreateContact';
+import ContactList from './ContactList';
+import PhoneList from './PhoneList';
+
 import './App.css';
 
 function App() {
+    const [contacts, setContacts] = useState([]);
+    const [selectedContact, setSelectedContact] = useState(null);
+
+    useEffect(() => {
+        axios.get('/api/contacts')
+            .then(response => {
+                setContacts(response.data);
+            })
+            .catch(error => {
+                console.error('Error fetching contacts:', error);
+            });
+    }, []);
+
+    const handleContactSelect = (contactId) => {
+        setSelectedContact(contactId);
+    };
+
+    const handleContactCreate = (newContact) => {
+        axios.post('/api/contacts', newContact)
+            .then(response => {
+                setContacts([...contacts, response.data]);
+            })
+            .catch(error => {
+                console.error('Error creating contact:', error);
+            });
+    };
+
+    const handleContactDelete = (contactId) => {
+        axios.delete(`/api/contacts/${contactId}`)
+            .then(() => {
+                const updatedContacts = contacts.filter(contact => contact.id !== contactId);
+                setContacts(updatedContacts);
+                setSelectedContact(null);
+            })
+            .catch(error => {
+                console.error('Error deleting contact:', error);
+            });
+    };
 
     return (
-        <div>     
+        <div>
             <h1>Contactor</h1>
+
+            <CreateContact onCreate={handleContactCreate} />
+
+            <ContactList
+                contacts={contacts}
+                onSelect={handleContactSelect}
+                onDelete={handleContactDelete}
+            />
+
+            {selectedContact && <PhoneList contactId={selectedContact} />}
         </div>
     );
 }
