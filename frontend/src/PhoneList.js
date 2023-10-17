780100d9765c1629948e9202eae20e3cd03b73ff
Arth <88418347+iiArthii@users.noreply.github.com>
2023-10-18 00:10:57 +1100

    phone list js created for storing phone info

---
+++ frontend/src/PhoneList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function PhoneList({ phones }) {
+    return (
+        <div>
+            <h2>Phone Numbers</h2>
+            <ul>
+                {phones.map((phone) => (
+                    <li key={phone.id}>
+                        {phone.name}: {phone.number}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default PhoneList;
