7a75c4745e35eb49c45a2ad3e3aef38aea38f723
Arth <88418347+iiArthii@users.noreply.github.com>
2023-10-17 23:40:18 +1100

    phone.controller changes to add functionality

--- api/controllers/phone.controller.js
+++ api/controllers/phone.controller.js
@@ -2,27 +2,98 @@ const db = require("../models");
 const Phones = db.phones;
 const Op = db.Sequelize.Op;
 
-// Create phone
 exports.create = (req, res) => {
-    
+    const { contactId } = req.params;
+    const { name, number } = req.body;
+
+    if (!name || !number) {
+        return res.status(400).send({ message: 'Name and number cannot be empty' });
+    }
+
+    Phones.create({
+        name,
+        number,
+        contactId,
+    })
+        .then(phone => {
+            res.send(phone);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while creating the phone number',
+            });
+        });
 };
 
-// Get all phones
 exports.findAll = (req, res) => {
-    
+    const { contactId } = req.params;
+
+    Phones.findAll({
+        where: { contactId },
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while retrieving phone numbers',
+            });
+        });
 };
 
-// Get one phone by id
 exports.findOne = (req, res) => {
-  
+    const { contactId, phoneId } = req.params;
+
+    Phones.findOne({
+        where: { id: phoneId, contactId },
+    })
+        .then(phone => {
+            if (!phone) {
+                return res.status(404).send({ message: 'Phone number not found' });
+            }
+            res.send(phone);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while retrieving the phone number',
+            });
+        });
 };
 
-// Update one phone by id
 exports.update = (req, res) => {
-    
+    const { contactId, phoneId } = req.params;
+    const { name, number } = req.body;
+
+    if (!name || !number) {
+        return res.status(400).send({ message: 'Name and number cannot be empty' });
+    }
+
+    Phones.update(
+        { name, number },
+        { where: { id: phoneId, contactId } }
+    )
+        .then(() => {
+            res.send({ message: 'Phone number updated successfully' });
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while updating the phone number',
+            });
+        });
 };
 
-// Delete one phone by id
 exports.delete = (req, res) => {
-    
+    const { contactId, phoneId } = req.params;
+
+    Phones.destroy({
+        where: { id: phoneId, contactId },
+    })
+        .then(() => {
+            res.send({});
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || 'Some error occurred while deleting the phone number',
+            });
+        });
 };
